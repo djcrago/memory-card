5ad47d949e44185bc3f878a5f08f36971b88d675
Dexter Crago <jodieanddexter@gmail.com>
2024-03-12 10:37:53 -0600

    Remove testing files

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import Info from './Info';
 import Scoreboard from './Scoreboard';
 import Cards from './Cards';
 import './styles/App.css';
-import Sandbox from './Sandbox';
-import Cards2 from './Cards2';
 
 function App() {
   const [clickedCards, setClickedCards] = useState([]);
@@ -14,7 +12,6 @@ function App() {
       <Info />
       <Scoreboard clickedCardsLength={clickedCards.length} />
       <Cards clickedCards={clickedCards} setClickedCards={setClickedCards} />
-      {/* <Cards2 clickedCards={clickedCards} setClickedCards={setClickedCards} /> */}
     </>
   );
 }
--- src/Card2.jsx
+++
@@ -1,9 +0,0 @@
-import './styles/Card.css';
-
-export default function Card2({ id, handleClick, url }) {
-  return (
-    <div className="card" id={id} onClick={handleClick}>
-      <img className="card-image" src={url} id={id} />
-    </div>
-  );
-}
