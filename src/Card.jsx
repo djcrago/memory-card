00c915b188a4cffe99e15be4f2ebc9929dd61a5c
Dexter Crago <jodieanddexter@gmail.com>
2024-03-11 19:21:03 -0600

    Populate cards with random dog images

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,9 +1,35 @@
+import { useState, useEffect } from 'react';
 import './styles/Card.css';
 
 export default function Card({ info, handleClick, id }) {
+  const [src, setSrc] = useState('');
+
+  useEffect(() => {
+    let ignore = false;
+
+    if (!ignore) {
+      getDog().then((result) => {
+        if (!ignore) {
+          setSrc(result);
+        }
+      });
+    }
+
+    return () => {
+      ignore = true;
+    };
+  }, []);
+
+  async function getDog() {
+    const response = await fetch('https://dog.ceo/api/breeds/image/random');
+    const message = await response.json();
+    return message.message;
+  }
+
   return (
     <div className="card" id={id} onClick={handleClick}>
-      {info}
+      <img className="card-image" src={src} id={id} />
+      <p id={id}>{info}</p>
     </div>
   );
 }
