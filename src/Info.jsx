3df5dc9d6cc1f57d5135089845ffa517e25eeb10
Dexter Crago <jodieanddexter@gmail.com>
2024-03-12 10:40:24 -0600

    Update info text

--- src/Info.jsx
+++ src/Info.jsx
@@ -3,11 +3,12 @@ import './styles/Info.css';
 export default function Info() {
   return (
     <>
-      <h1>Memory Game</h1>
+      <h1>Dog Memory Game</h1>
       <p>
-        Get points by clicking on an image but don&apos;t click on any more than
-        once!
+        Get points by clicking on a random image of a dog, but don&apos;t click
+        on any more than once!
       </p>
+      <p>(It may take a couple seconds for the images to appear)</p>
     </>
   );
 }
